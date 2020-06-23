eb46209c3bf0e3e9980e06487cb9045700cbd1cd
Usman Malik <usmanmalik9797@gmail.com>
2020-06-23 13:29:55 +1000

    Stable version with authentication and navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Auth from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react";
 import "@aws-amplify/ui/dist/style.css";
+import Header from "./Components/Header";
+import Users from "./Components/Users";
 Auth.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <body className="App">
+        <div>
+          <Header />
+          <div>
+            <Users />
+          </div>
+        </div>
+      </body>
+    </>
   );
 }
 
---
+++ src/Components/Users.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { Component } from "react";
+
+class Users extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Users;
